c5e68efddca79816b86dee20d0fe1b9ec9a5366b
Steven Pears <steven.pears@live.co.uk>
2018-02-24 16:31:54 +0000

    Update skill manifest using textbox

--- Alexa.NET.Management.UWPApp/MainPage.xaml.cs
+++ Alexa.NET.Management.UWPApp/MainPage.xaml.cs
@@ -1,8 +1,16 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Notifications;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 using Alexa.NET.Management.Api;
+using Alexa.NET.Management.Manifest;
 using Alexa.NET.Management.Skills;
 using Alexa.NET.Management.UWPApp.Utility;
 using Alexa.NET.Management.Vendors;
@@ -19,7 +27,12 @@ namespace Alexa.NET.Management.UWPApp
     public sealed partial class MainPage : Page
     {
         private ManagementApi Api { get; set; }
-        public SkillSummary CurrentSkill { get; set; }
+        public Skill CurrentSkill { get; set; }
+
+        private JsonSerializer Serializer { get; } = JsonSerializer.CreateDefault(new JsonSerializerSettings
+        {
+            Converters = new List<JsonConverter> { new Internals.ApiConverter() }
+        });
 
         public MainPage()
         {
@@ -48,7 +61,7 @@ namespace Alexa.NET.Management.UWPApp
         private async Task GetAndSetSkills(string vendorId)
         {
             var skillList = await Api.Skills.List(vendorId);
-            SkillNav.MenuItemsSource = skillList.Skills;
+            SkillNav.MenuItemsSource = skillList.Skills.OrderBy(s => s.NameByLocale[SkillItemTitleConverter.Locale]).ThenBy(s => s.Stage.ToString());
         }
 
         private void SetVendor(Vendor vendor)
@@ -59,18 +72,69 @@ namespace Alexa.NET.Management.UWPApp
 
         private async void SkillNav_OnSelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
         {
-            if (!(args.SelectedItem is SkillSummary summary))
+            await SetSkill(args.SelectedItem as SkillSummary);
+        }
+
+        private async void RefreshSkill(object sender, RoutedEventArgs e)
+        {
+            await SetSkill(SkillNav.SelectedItem as SkillSummary);
+        }
+
+        public async void UpdateSkill(object sender, RoutedEventArgs e)
+        {
+            if (!(SkillNav.SelectedItem is SkillSummary summary))
             {
-                SkillNav.Content = null;
                 return;
             }
 
-            var skill = await Api.Skills.Get(summary.SkillId, summary.Stage.ToString().ToLower());
+            if (summary.Stage == SkillStage.LIVE)
+            {
+                var dialog = new MessageDialog("This will update the development version of this skill, is this okay?",
+                    "Unable to edit live skill");
 
+                var result = false;
+                dialog.Commands.Add(new UICommand("OK",a => result = true));
+                dialog.Commands.Add(new UICommand("Cancel",a=>result = false));
+                await dialog.ShowAsync().AsTask();
+                if (!result)
+                {
+                    return;
+                }
+            }
+
+            var text = ((ScrollViewer) InfoTab.Content).Content as TextBox;
+            Skill skill;
+            using (var reader = new JsonTextReader(new StringReader(text.Text)))
+            {
+                skill = Serializer.Deserialize<Skill>(reader);
+            }
+
+            try
+            {
+                await Api.Skills.Update(summary.SkillId, SkillStage.DEVELOPMENT.ToString(),skill);
+                await SetSkill(summary);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private async Task SetSkill(SkillSummary summary)
+        {
+            var skill = await Api.Skills.Get(summary.SkillId, summary.Stage.ToString().ToLower());
+            CurrentSkill = skill;
             var output = new ScrollViewer();
 
-            var textview = new TextBlock {Text = JObject.FromObject(skill.Manifest).ToString(Formatting.Indented)};
+            var textview = new TextBox {AcceptsReturn = true,TextWrapping = TextWrapping.Wrap};
             output.Content = textview;
+
+            var osb = new StringBuilder();
+            using (var textWriter = new JsonTextWriter(new StringWriter(osb)))
+            {
+                Serializer.Serialize(textWriter, skill);
+            }
+
+            textview.Text = osb.ToString();
 
             InfoTab.Content = output;
         }
